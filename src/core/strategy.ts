f2f4a685c8a66debf84271b884fa10c80220245a
Hemanta Sundaray <pgcim14.hemant@spjimr.org>
2025-11-03 11:21:57 +0530

    added core strategy interface

---
+++ src/core/strategy.ts
@@ -0,0 +1,18 @@
+import type { AuthConfig } from '../config/schema';
+
+export interface BaseSignInOptions {
+  redirectTo?: `/${string}`;
+}
+
+export interface OAuthStrategy<
+  Config,
+  Options extends BaseSignInOptions,
+  Result,
+> {
+  config: AuthConfig;
+  providerConfig: Config;
+  signin(options: Options): Promise<never>;
+  handleCallback(request: Request): Promise<Result>;
+}
+
+export type AnyAuthStrategy = OAuthStrategy<any, any, any>;
