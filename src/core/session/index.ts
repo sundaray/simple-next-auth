14bd56627991fb6f43624ba034ebf8059687af57
Hemanta Sundaray <pgcim14.hemant@spjimr.org>
2025-11-11 11:56:27 +0530

    removed set user session function

--- src/core/session/index.ts
+++ src/core/session/index.ts
@@ -1,5 +1,4 @@
 export { encryptUserSessionPayload } from './encrypt-user-session-payload.js';
-export { setUserSessionCookie } from './set-user-session-cookie.js';
 export { decryptUserSessionJWE } from './decrypt-user-session-jwe.js';
 export { deleteOAuthStateCookie } from './delete-oauth-state-cookie.js';
 export { createUserSessionPayload } from './create-user-session-payload.js';
